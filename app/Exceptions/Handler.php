9f9af863d596369c4ec8a6c219439986fb3565b0
Kelvin Chikezie <kevchike@gmail.com>
2022-08-09 13:31:57 +0100

    Handle general 404 and 405 errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,13 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -49,6 +51,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Resource not found. Check your endpoint properly',
+            ], 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            $method = request()->method();
+
+            return response()->json([
+                'status' => 'error',
+                'message' => "You can not use $method method to access this endpoint",
+            ], 405);
+        }
+
         return parent::render($request, $exception);
     }
 }
